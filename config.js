81ef0ff382d4bf96234eb4265d231b104af8fb6b
nicov <n.villalonga@globant.com>
2015-06-02 21:30:58 -0300

    config agus

--- config.js
+++ config.js
@@ -26,10 +26,10 @@ module.exports = {
 
 /** Conexion a MongoLab */
 
-var user = "gadb";
-var password = "probandom0ng0";
+var user = "agusburgos";
+var password = "aprendiendo";
 //var address = "@ds031882.mongolab.com:31882/meanapp";
-var address = "@ds045057.mongolab.com:43200/gadb";
+var address = "@ds043200.mongolab.com:43200/aprendiendo";
 
 var url = 'mongodb://' + user + ':' + password + address;
 
@@ -38,8 +38,8 @@ var options = {
   server: { poolSize: 5, 
             keepAlive: 1 },
   replset: { rs_name: 'myReplicaSetName' },
-  user: 'gadb',
-  pass: 'probandom0ng0'
+  user: 'agusburgos',
+  pass: 'aprendiendo'
 } 
 
 mongoose.connect(url);
