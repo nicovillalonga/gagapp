415bc950efdac209f413dd9aa4882d6a02f1d391
Agustin Burgos <agusburgos1602@gmail.com>
2016-05-30 20:38:03 -0300

    Implemented Api Docs for the back end

---
+++ apidoc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "gagapp",
+  "version": "0.1.0",
+  "description": "Project: gagapp Api documentation for Utn Tesis by Agustin Burgos & Nicolas Villalonga",
+  "title": "API Docs",
+  "url": "localhost:8080/api",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-05-30T23:35:18.153Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
