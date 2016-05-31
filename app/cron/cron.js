0af15554a1d7fc41ad0eaa27f34def35c1dd9f40
agusburgos <agusburgos1602@gmail.com>
2016-05-30 23:02:13 -0300

    added apidoc ignore

--- app/cron/cron.js
+++ app/cron/cron.js
@@ -2,7 +2,15 @@ var cronJob = require('cron').CronJob;
 var users = require('./users');
 var config = require('../../config');
 
-//Runs every Monday at 03:00 a.m. 
+/**
+ * @api {get} /cron Delete inactives users
+ * @apiName Cron Delete Users
+ * @apiGroup Cron
+ * @apiDescription Runs every Monday at 03:00 a.m.
+ *
+ * @apiSuccess {String} message List all users deleted.
+ */
+//Runs every Monday at 03:00 a.m.
 new cronJob('00 00 03 * * 1', function(){
   try {    
     users();
